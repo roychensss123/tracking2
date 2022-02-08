e4a1d210fc8f299f62f5a03c59cfdf1d0a853497
roychensss123 <99218921+roychensss123@users.noreply.github.com>
2022-02-08 10:24:48 -0600

    Create form.js

---
+++ form.js
@@ -0,0 +1,14 @@
+<script>
+
+var queryString = window.location.search;
+
+var urlParams = new URLSearchParams(queryString);
+var is_campaign = urlParams.get('utm_campaign');
+var is_source = urlParams.get('utm_source');
+var is_medium = urlParams.get('utm_medium');
+
+document.getElementById('100432765').value = is_source;
+document.getElementById('100488551').value = is_medium;
+document.getElementById('100488555').value = is_campaign;
+
+</script>
