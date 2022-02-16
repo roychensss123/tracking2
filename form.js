1e933bf51fb0bba31da059a099922f096ceae0d5
roychensss123 <99218921+roychensss123@users.noreply.github.com>
2022-02-16 14:59:51 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,11 +1,12 @@
-var queryString = window.location.search;
 
+function startScript() {
+var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var is_campaign = urlParams.get('utm_campaign');
 var is_source = urlParams.get('utm_source');
 var is_medium = urlParams.get('utm_medium');
-
 document.getElementById('100432765').value = is_source;
 document.getElementById('100488551').value = is_medium;
 document.getElementById('100488555').value = is_campaign;
-
+}
+window.onclick = startScript;
