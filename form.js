fd2e1450d98a4fea87642ebfbf11d6b9768969b2
roychensss123 <99218921+roychensss123@users.noreply.github.com>
2022-02-28 14:33:36 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,12 +1,11 @@
-
-function startScript() {
-var queryString = window.location.search;
-var urlParams = new URLSearchParams(queryString);
-var is_campaign = urlParams.get('utm_campaign');
-var is_source = urlParams.get('utm_source');
-var is_medium = urlParams.get('utm_medium');
-document.getElementById('100432765').value = is_source;
-document.getElementById('100488551').value = is_medium;
-document.getElementById('100488555').value = is_campaign;
-}
+function startScript() 
+{ 
+var queryString = window.location.search; 
+var urlParams = new URLSearchParams(queryString); 
+var is_campaign = urlParams.get('utm_campaign'); 
+var is_source = urlParams.get('utm_source'); 
+var is_medium = urlParams.get('utm_medium'); 
+loader.engine.document.getElementById(100432765).setValue({ value: is_campaign}); loader.engine.document.getElementById(100488551).setValue({ value: is_source}); loader.engine.document.getElementById(100488555).setValue({ value: is_medium}); 
+} 
 window.onclick = startScript;
+
